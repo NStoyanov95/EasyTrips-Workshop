2a19070fc5040fb73c1814fd119902207d388168
NStoyanov95 <nstoyanov9512@gmail.com>
2024-01-14 14:42:54 +0200

    Add comment model

---
+++ src/models/Comment.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    comment: String,
+});
+
+const Comment = mongoose.model('Comment', commentSchema);
+
+module.exports = Comment;
