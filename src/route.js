3296e76c995618fef7cae12de12fa3b3e2381f51
NStoyanov95 <nstoyanov9512@gmail.com>
2024-01-24 09:30:40 +0200

    Add login page

--- src/route.js
+++ src/route.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const tripController = require('./controllers/tripController');
 const commentController = require('./controllers/commentController');
+const userController = require('./controllers/userController');
 
 router.use('/', homeController);
 router.use('/trips', tripController);
 router.use('/comments', commentController);
+router.use('/users', userController);
 
 module.exports = router;
