276c6cf1b099ad54c12e1203853562b8cf35b06c
NStoyanov95 <nstoyanov9512@gmail.com>
2024-01-28 14:21:31 +0200

    Validate when login and return token

---
+++ src/lib/constants.js
@@ -0,0 +1,3 @@
+const SECRET = '5qfhIkKruwsJ0l0K'
+
+module.exports = SECRET;
--- src/manager/userManager.js
+++ src/manager/userManager.js
@@ -1,3 +1,30 @@
+const { MongooseError } = require('mongoose');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+const SECRET = require('../lib/constants');
 
 exports.register = (userData) => User.create(userData);
+
+
+exports.login = async (username, password) => {
+    const user = await User.findOne({ username });
+
+    if (!user) {
+        throw new MongooseError('Invalid username or password!');
+    };
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new MongooseError('Invalid username or password!');
+    };
+
+    const payload = {
+        _id: user._id,
+        username: user.username,
+    };
+
+    const token = jwt.sign(payload, SECRET, {expiresIn: '1d'});
+    return token;
+}
